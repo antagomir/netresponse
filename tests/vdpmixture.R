321e4b13f83c3d6ee6844e19f15d86765ff0a1be
Leo Lahti <leo.lahti@tkk.fi>
2010-10-11 22:10:00 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netresponse@50107 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/vdpmixture.R
+++ tests/vdpmixture.R
@@ -14,15 +14,51 @@ library(netresponse)
 
 #dyn.load("/home/tuli/Rpackages/netresponse/netresponse/src/netresponse.so")
 
+
+###################################################################
+
+
+
+ellipse <-
+  function (x, scale = c(1, 1), centre = c(0, 0), level = 0.95, 
+            t = sqrt(qchisq(level, 2)), which = c(1, 2), npoints = 100, ...) 
+{
+
+  # Modified from the 'ellipse.default' function of ellipse package (version 0.3-5) in CRAN
+
+  names <- c("x", "y")
+  if (is.matrix(x)) {
+    xind <- which[1]
+    yind <- which[2]
+    r <- x[xind, yind]
+    if (missing(scale)) {
+      scale <- sqrt(c(x[xind, xind], x[yind, yind]))
+      if (scale[1] > 0) r <- r/scale[1]
+      if (scale[2] > 0) r <- r/scale[2]
+    }
+    if (!is.null(dimnames(x)[[1]])) 
+      names <- dimnames(x)[[1]][c(xind, yind)]
+  }
+  else r <- x
+  r <- min(max(r,-1),1)  # clamp to -1..1, in case of rounding errors
+  d <- acos(r)
+  a <- seq(0, 2 * pi, len = npoints)
+  matrix(c(t * scale[1] * cos(a + d/2) + centre[1], t * scale[2] * 
+           cos(a - d/2) + centre[2]), npoints, 2, dimnames = list(NULL, 
+                                                    names))
+}
+
+
+
 #########  GENERATE DATA #############################################
 
 # Generate Nc components from normal-inverseGamma prior
 
-set.seed(1234)
+set.seed(12346)
 
 dd <- 2   # Dimensionality of data
-Nc <- 3   # Number of components
-Ns <- 100 # Number of data points
+Nc <- 5   # Number of components
+Ns <- 200 # Number of data points
 sd0 <- 3  # component spread
 rgam.shape = 2 # parameters for Gamma distribution 
 rgam.scale = 2 # parameters for Gamma distribution to define precisions
@@ -64,19 +100,40 @@ ord.in <- order(component.means[,1])
 means.out <- out$posterior$centroids[ord.out,]
 means.in <- component.means[ord.in,]
 
-# Cluster variances
+# Cluster stds and variances
 sds.out <- out$posterior$sds[ord.out,]
 sds.in  <- sqrt(component.vars[ord.in,])
+vars.out <- sds.out^2
+vars.in <- sds.in^2
 
 # Check correspondence between input and output
-cm <- cor(as.vector(means.in), as.vector(means.out))
-csd <- cor(as.vector(sds.in), as.vector(sds.out))
+if (length(means.in) == length(means.out)) {
+   cm <- cor(as.vector(means.in), as.vector(means.out))
+   csd <- cor(as.vector(sds.in), as.vector(sds.out))
+}
 
 # Plot results (assuming 2D)
-plot(D, pch = 20, main = paste("Cor.means:", round(cm,3), "/ Cor.sds:", round(csd,3))) 
-for (ci in 1:Nc)  {
+
+ran <- range(c(as.vector(means.in - 2*vars.in), 
+               as.vector(means.in + 2*vars.in), 
+	       as.vector(means.out + 2*vars.out), 
+	       as.vector(means.out - 2*vars.out)))
+
+plot(D, pch = 20, main = paste("Cor.means:", round(cm,3), "/ Cor.sds:", round(csd,3)), xlim = ran, ylim = ran) 
+
+for (ci in 1:nrow(means.out))  {
     points(means.out[ci,1], means.out[ci,2], col = "red", pch = 19)
-    points(means.in[ci,1], means.in[ci,2], col = "blue", pch = 19)
+    el <- ellipse(matrix(c(vars.out[ci,1],0,0,vars.out[ci,2]),2), centre = means.out[ci,])
+    lines(el, col = "red") 						  
 }
+
+for (ci in 1:nrow(means.in))  {
+    points(means.in[ci,1], means.in[ci,2], col = "blue", pch = 19)
+    el <- ellipse(matrix(c(vars.in[ci,1],0,0,vars.in[ci,2]),2), centre = means.in[ci,])
+    lines(el, col = "blue") 						  
+}
+
+
+
 
 
