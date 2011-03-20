413d7fec2948e38b1fd622fa85f0a680bf8cc2d3
l.lahti <l.lahti@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-03-20 23:33:02 +0000

    fixed bug in get.subnets.Rd
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netresponse@53911 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,4 +1,5 @@
 setGeneric("get.subnets", function( model, get.names = TRUE, min.size = 2, max.size = Inf, min.responses = 2){ standardGeneric ("get.subnets") })
+setGeneric("get.qofz", function( model, subnet.id, log = FALSE){ standardGeneric ("get.qofz") })
 #setGeneric("get.model.parameters", function( model, ... ){ standardGeneric ("get.model.parameters") })
 
 
