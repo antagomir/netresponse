4c59748a6c6f22619b5ba93983c98f0fe5f5d05a
Leo Lahti <leo.lahti@tkk.fi>
2011-04-07 18:14:39 +0000

    network node removal made faster
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netresponse@54606 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.network.R
+++ R/read.network.R
@@ -1,14 +1,36 @@
-read.sif <- function (sif.file, format = "graphNEL", directed = FALSE) 
+read.sif <- 
+function (sif.file, format = "graphNEL", directed = FALSE) 
 {
-  net <- read.table(file = sif.file, colClasses = "character")
-  net <- graph.edgelist(as.matrix(net[, -2]), directed = directed)
 
-  if (format == "graphNEL") {
-    net <- igraph.to.graphNEL(net)
-  }
+    net <- read.csv(file = sif.file, sep = "\t", colClasses = "character")
+        
+    # assume form: node1 linktype node2 side.info..
+    if ( ncol(net) > 2 ) { 
 
-  net
+      # remove NA nodes 
+      nas <- apply(net, 1, function (x) {any(is.na(x[c(1,3)]))})
+      if (any(nas)) {
+        net <- net[!nas, ]
+        warning("NAs removed from network node list, ", sum(nas), " edges removed.")
+      }
+      
+      net <- graph.edgelist(as.matrix(net[, -2]), directed = directed)
 
+    } else if ( ncol(net) == 2 ) { # assume form: node1 node2
+
+      # remove NA nodes 
+      nas <- apply(net, 1, function (x) {any(is.na(x))})
+      if (any(nas)) {
+        net <- net[!nas, ]
+        warning("NAs removed from network node list, ", sum(nas), " edges removed.")
+      }
+      
+      net <- graph.edgelist(cbind(net[,1],net[,2]), directed = directed)
+    }
+
+    if (format == "graphNEL") { net <- igraph.to.graphNEL(net) }
+
+    net
 }
 
 #library(graph)
