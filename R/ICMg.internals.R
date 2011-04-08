873f17b275199533c56ef189965356cacfc55ea3
j.parkkinen <j.parkkinen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-04-08 07:21:23 +0000

    Updated ICMg documentation
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netresponse@54622 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ICMg.internals.R
+++ R/ICMg.internals.R
@@ -1,3 +1,17 @@
+#
+# Copyright (C) 2008-2011 Juuso Parkkinen
+# Contact: Juuso Parkkinen <juuso.parkkinen@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 `ICMg.combined.iteration` <-
 function(L, X, Niter, N, M, Lindices, Nindices,
                         D, C, z, w, n, m, q, E,
