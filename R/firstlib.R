761f2ba0a16fb5505eb69cf1f21bd8e6309b1590
Leo Lahti <leo.lahti@tkk.fi>
2013-10-20 10:42:53 +0000

    netresponse for bioc devel
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netresponse@81892 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/firstlib.R
+++ R/firstlib.R
@@ -11,8 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-.onLoad <- function(lib, pkg)
+.onAttach <- function(lib, pkg)
 {
-   #library.dynam('netresponse', pkg, lib)
    packageStartupMessage('\nnetresponse Copyright (C) 2008-2013 Leo Lahti.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under GNU GPL >=2, see the licensing terms for details.\n')
 }
