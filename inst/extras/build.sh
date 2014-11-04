310e85425884899a04fe89c3afa5665b04334061
l.lahti <l.lahti@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-11-04 12:59:03 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 1767ad20302265ea86a302cbc8566f10256d8919
    
        R
    
    Committed by: antagomir
    Author Name: antagomir
    Commit date: 2014-11-04 13:58:55 +0100
    Author date: 2014-11-04 13:58:55 +0100
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netresponse@96420 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,7 +1,7 @@
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check netresponse_1.17.11.tar.gz
-/usr/bin/R CMD INSTALL netresponse_1.17.11.tar.gz
-#/usr/bin/R CMD BiocCheck netresponse_1.17.1.tar.gz 
+/usr/bin/R CMD check netresponse_1.17.12.tar.gz
+/usr/bin/R CMD INSTALL netresponse_1.17.12.tar.gz
+#/usr/bin/R CMD BiocCheck netresponse_1.17.12.tar.gz 
 
 
