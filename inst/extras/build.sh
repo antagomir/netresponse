ba55c42021ca9ac283f2d312e3cb2ff50493bcbf
antagomir <leo.lahti@iki.fi>
2016-04-03 12:09:58 +0200

    Fixed devel build problems

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,5 +1,5 @@
 #/usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check netresponse_1.21.15.tar.gz
-/usr/bin/R CMD INSTALL netresponse_1.21.15.tar.gz
+/usr/bin/R CMD check --as-cran netresponse_1.3.16.tar.gz
+/usr/bin/R CMD INSTALL netresponse_1.3.16.tar.gz
 #/usr/bin/R CMD BiocCheck netresponse_1.17.13.tar.gz 
