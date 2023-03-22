ba7c0c519a527db03041094731d0bc8031adb25d
antagomir <leo.lahti@iki.fi>
2023-03-22 17:55:52 +0200

    updated version number and running checks

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,7 +1,7 @@
 #~/bin/R-patched/bin/R CMD BATCH document.R4.2.2
-~/bin/R-4.2.1/bin/R CMD build ../../ --resave-data #--no-examples  --no-build-vignettes 
-~/bin/R-4.2.1/bin/R CMD check netresponse_1.57.2.tar.gz #--no-build-vignettes --no-examples
-#~/bin/R-4.2.1/bin/R CMD check --as-cran netresponse_1.57.2.tar.gz
-~/bin/R-4.2.1/bin/R CMD BiocCheck netresponse_1.57.2.tar.gz
-~/bin/R-4.2.1/bin/R CMD INSTALL netresponse_1.57.2.tar.gz 
-#~/bin/R-4.2.1/bin/R CMD BiocCheck netresponse_1.17.13.tar.gz 
+~/bin/R-4.2.2/bin/R CMD build ../../ --resave-data #--no-examples  --no-build-vignettes 
+~/bin/R-4.2.2/bin/R CMD check netresponse_3.17.1.tar.gz #--no-build-vignettes --no-examples
+#~/bin/R-4.2.2/bin/R CMD check --as-cran netresponse_3.17.1.tar.gz
+~/bin/R-4.2.2/bin/R CMD BiocCheck netresponse_3.17.1.tar.gz
+~/bin/R-4.2.2/bin/R CMD INSTALL netresponse_3.17.1.tar.gz 
+#~/bin/R-4.2.2/bin/R CMD BiocCheck netresponse_1.17.13.tar.gz 
